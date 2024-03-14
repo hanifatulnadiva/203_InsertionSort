c0584e985db26cb5daaa7c3e3658927b38adfa9b
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-03-14 14:33:50 +0700

    membuat variabel dan step 1

--- 203_InsertionSort/203_InsertionSort.cpp
+++ 203_InsertionSort/203_InsertionSort.cpp
@@ -2,6 +2,16 @@
 //
 
 #include <iostream>
+using namespace std;
+
+void insertionSort() {
+    int i, j, n;
+    int arr[20];
+
+    for (i = 1; i <= n - 1; i++) {  //step 1
+
+    }
+}
 
 int main()
 {
