7d89c08ef84f594dc6ec55db0ff72ba18518f46b
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-03-14 15:03:22 +0700

    menambahkan variabel temp, step 1, steep 2, step 3, step4,step 4 a, step 4b, step 5

--- 203_InsertionSort/203_InsertionSort.cpp
+++ 203_InsertionSort/203_InsertionSort.cpp
@@ -5,11 +5,21 @@
 using namespace std;
 
 void insertionSort() {
-    int i, j, n;
+    int i, j, n, temp;
     int arr[20];
 
     for (i = 1; i <= n - 1; i++) {  //step 1
+        temp = arr[i];    //step 2
 
+        j = i - 1; //step 3
+
+        while (j >= 0 && arr[j] > temp) //step 4
+        {
+            arr[j + 1] = arr[j]; // step 4a
+            j--; // step 4b
+
+        }
+        arr[j + 1] = temp;    //step 5
     }
 }
 
